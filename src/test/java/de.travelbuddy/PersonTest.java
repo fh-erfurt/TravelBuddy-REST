2015c1a821a424e88da526237b95e84347cb8f5e
Marcel van der Heide <fd@nordrassil.de>
2019-12-03 13:55:49 +0100

    Beispieltest für Person angelegt

--- src/test/java/de.travelbuddy/PersonTest.java
+++ src/test/java/de.travelbuddy/PersonTest.java
@@ -1,5 +1,21 @@
 package de.travelbuddy;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 public class PersonTest {
+    @Test
+    public void correctly_instatiate_person()
+    {
+        String firstname = "Marcel";
+        String name = "van der Heide";
+        LocalDate bday = LocalDate.of(1990,5,2);
+
+        Person person = new Person(firstname, name, bday);
+        
+        assertEquals(person.getFirstName(), firstname);
+        assertEquals(person.getName(), name);
+        assertEquals(person.getBirthdate(), bday);
+    }
 }
