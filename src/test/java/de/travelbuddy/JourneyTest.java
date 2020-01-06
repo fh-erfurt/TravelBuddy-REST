70bee21b2de1a020ede5105e8fa954c9e7b7fe6e
Franziska <franziska.schmidt.1@fh-erfurt.de>
2020-01-06 13:04:15 +0100

    ConnectionTest fertig

--- src/test/java/de/travelbuddy/JourneyTest.java
+++ src/test/java/de/travelbuddy/JourneyTest.java
@@ -1,19 +1,29 @@
 package de.travelbuddy;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import de.travelbuddy.place.Place;
 import org.junit.jupiter.api.Test;
 import de.travelbuddy.Journey;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JourneyTest {
 
     @Test
     public void correctly_instantiate_journey(){
-        //Given
 
-        //When
+        String title = "Trip";
+        List<Place> places = new ArrayList<>();
+        List<Person> persons = new ArrayList<>();
 
-        //Then
+        Journey journey = new Journey(title, places, persons);
+
+        assertEquals(journey.getTitle(), title);
+        assertEquals(journey.getPlaces(), places);
+        assertEquals(journey.getPersons(), persons);
     }
-
-
 }
+
