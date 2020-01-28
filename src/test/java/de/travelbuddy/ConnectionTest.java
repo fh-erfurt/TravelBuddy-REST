455969d6d09a953bb180d701ffbe6ac4bf941153
Franziska <franziska.schmidt.1@fh-erfurt.de>
2020-01-28 22:27:03 +0100

    Test erweitert ConnectionTest, ContactDetailTest, SightTest

--- src/test/java/de/travelbuddy/ConnectionTest.java
+++ src/test/java/de/travelbuddy/ConnectionTest.java
@@ -17,7 +17,7 @@ import java.util.Currency;
 
 public class ConnectionTest {
     @Test
-    public void should_create_connection() {
+    public void should_create_connectionBerlin() {
         String title = "Berlin";
         LocalDateTime arrive = LocalDateTime.of(2020,7,25,16,45) ;
         LocalDateTime departure = LocalDateTime.of(2020,7,28,20,5) ;
@@ -29,11 +29,139 @@ public class ConnectionTest {
                 new Coordinates("52.516181", "13.376935"),
                 contact,
                 arrive,departure, new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(),
-                new ArrayList<>(), Accommodation.accommodationType.HOTEL);
+                new ArrayList<>(), Accommodation.accommodationType.HOSTEL);
 
         Place end = new Sight("Brandenburger Tor", new Coordinates("52.516275", "13.377704"),
                 new ContactDetails("9999999", "brandeburger@tor.de", "Berlin",
                         "Pariser Platz", 1, "10117", "Deutschland"), arrive, departure,
+                new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(), new ArrayList<>(), false);
+
+
+        Connection connection = new Connection(title, arrive, departure, start, end, expense1);
+
+        assertEquals(connection.getTitle(), title);
+        assertEquals(connection.getArrive(), arrive);
+        assertEquals(connection.getDeparture(), departure);
+        assertEquals(connection.getStart(), start);
+        assertEquals(connection.getEnd(), end);
+        assertEquals(connection.getExpense(), expense1);
+
+    }
+
+    @Test
+    public void should_create_connectionHarz() {
+        String title = "Harz";
+        LocalDateTime arrive = LocalDateTime.of(2020,8,13,8,13) ;
+        LocalDateTime departure = LocalDateTime.of(2020,8,31,13,8) ;
+        ContactDetails contact = new ContactDetails("039455 58817", "INFO@zelten.de", "Elend",
+                " Am Stern", 1, "38875", "Germany" );
+        Expense expense1 = new Expense("Zelten", "OpenAir", new Money(Currency.getInstance("EUR"),
+                new BigDecimal(35)), new ArrayList<>(), Expense.planned.PLANNED, true );
+        Place start = new Accommodation("Harz Camping am Schierker Stern",
+                new Coordinates("51.7569015", "10.6832768"),
+                contact,
+                arrive,departure, new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(),
+                new ArrayList<>(), Accommodation.accommodationType.CAMPING);
+
+        Place end = new Sight("Brocken", new Coordinates("51.806323199999994", "10.631225899999999"),
+                new ContactDetails("039455 120", "verwaltung@brocken.harz", "Wernigerode",
+                        "Harz", 1, "38879 ", "Deutschland"), arrive, departure,
+                new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(), new ArrayList<>(), false);
+
+
+        Connection connection = new Connection(title, arrive, departure, start, end, expense1);
+
+        assertEquals(connection.getTitle(), title);
+        assertEquals(connection.getArrive(), arrive);
+        assertEquals(connection.getDeparture(), departure);
+        assertEquals(connection.getStart(), start);
+        assertEquals(connection.getEnd(), end);
+        assertEquals(connection.getExpense(), expense1);
+
+    }
+
+    @Test
+    public void should_create_connectionUnterfoehring() {
+        String title = "Unterföhring";
+        LocalDateTime arrive = LocalDateTime.of(2020,2,06,18,00) ;
+        LocalDateTime departure = LocalDateTime.of(2020,2,10,14,30) ;
+        ContactDetails contact = new ContactDetails(" +498192 933417", "contact@mti.tv", "Unterföhring",
+                "Beta-Str.", 1, "85774", "Germany" );
+        Expense expense1 = new Expense("Ausflug", "Wandertag", new Money(Currency.getInstance("EUR"),
+                new BigDecimal(35)), new ArrayList<>(), Expense.planned.PLANNED, true );
+        Place start = new Accommodation("MTI Teleport München GmbH",
+                new Coordinates("48.1900696", "11.6569066"),
+                contact,
+                arrive,departure, new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(),
+                new ArrayList<>(), Accommodation.accommodationType.AIRBNB);
+
+        Place end = new Sight("Feringasee", new Coordinates("48.1981235", "11.6755371"),
+                new ContactDetails("089 9503636", "Beach@Volleyball.sport", "Unterföhring",
+                        "Am Feringasee", 22, "85774 ", "Deutschland"), arrive, departure,
+                new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(), new ArrayList<>(), false);
+
+
+        Connection connection = new Connection(title, arrive, departure, start, end, expense1);
+
+        assertEquals(connection.getTitle(), title);
+        assertEquals(connection.getArrive(), arrive);
+        assertEquals(connection.getDeparture(), departure);
+        assertEquals(connection.getStart(), start);
+        assertEquals(connection.getEnd(), end);
+        assertEquals(connection.getExpense(), expense1);
+
+    }
+
+    @Test
+    public void should_create_connectionCantor() {
+        String title = "CantorPfad";
+        LocalDateTime arrive = LocalDateTime.of(1845,3,3,5,7) ;
+        LocalDateTime departure = LocalDateTime.of(1918,1,6,9,11) ;
+        ContactDetails contact = new ContactDetails("0345 6903156", "leitung(at)cantor.de", "Halle (Saale)",
+                "Torstraße", 13, "06110", "Germany" );
+        Expense expense1 = new Expense("TimeTravel", "History", new Money(Currency.getInstance("RUB"),
+                new BigDecimal(35)), new ArrayList<>(), Expense.planned.CANCELED, false );
+        Place start = new Accommodation("Geburtsort Sankt Petersburg",
+                new Coordinates("59.9342802", "30.3350986"),
+                contact,
+                arrive,departure, new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(),
+                new ArrayList<>(), Accommodation.accommodationType.COUCHSURF);
+
+        Place end = new Sight("Lebensabend Halle", new Coordinates("51.4727992", "11.832289"),
+                new ContactDetails("089 9503636", "Beach@Volleyball.sport", "Halle (Saale)",
+                        "Delitzscher Str.", 70, "06112 ", "Deutschland"), arrive, departure,
+                new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(), new ArrayList<>(), false);
+
+
+        Connection connection = new Connection(title, arrive, departure, start, end, expense1);
+
+        assertEquals(connection.getTitle(), title);
+        assertEquals(connection.getArrive(), arrive);
+        assertEquals(connection.getDeparture(), departure);
+        assertEquals(connection.getStart(), start);
+        assertEquals(connection.getEnd(), end);
+        assertEquals(connection.getExpense(), expense1);
+
+    }
+
+    @Test
+    public void should_create_connectionPisa() {
+        String title = "Pisa";
+        LocalDateTime arrive = LocalDateTime.of(2023,4,3,12,34) ;
+        LocalDateTime departure = LocalDateTime.of(2023,4,5,23,59) ;
+        ContactDetails contact = new ContactDetails("0112358/1321345589", "fibo@nacci.it", "Pisa",
+                "Via Carlo Francesco Gabba", 17, "56122 ", "Italien" );
+        Expense expense1 = new Expense("Sight", "Cultural", new Money(Currency.getInstance("EUR"),
+                new BigDecimal(35)), new ArrayList<>(), Expense.planned.ISSUED, true );
+        Place start = new Accommodation("Grand Hotel Bonanno",
+                new Coordinates("43.7199624", "10.3840334"),
+                contact,
+                arrive,departure, new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(),
+                new ArrayList<>(), Accommodation.accommodationType.HOTEL);
+
+        Place end = new Sight("Torre di Pisa", new Coordinates("43.722952", "10.3944083"),
+                new ContactDetails("089 9503636", "look@opapisa.it", "Pisa",
+                        "Piazza del Duomo", 0, "56126  ", "Italien"), arrive, departure,
                 new ArrayList<>(Arrays.asList(expense1)), new ArrayList<>(), new ArrayList<>(), false);
 
 
--- src/test/java/de/travelbuddy/ContactDetailsTest.java
+++ src/test/java/de/travelbuddy/ContactDetailsTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ContactDetailsTest {
 
     @Test
-    public void correctly_instantiate_ContactDetails(){
+    public void correctly_instantiate_ContactDetailsFHEf(){
 
         String town = "Erfurt";
         String street = "Altonaer Straße";
@@ -15,6 +15,121 @@ public class ContactDetailsTest {
         String country = "Deutschland";
         String phone = "036167000";
         String email = "rektorat@fh-erfurt.de";
+
+        ContactDetails contact = new ContactDetails(phone, email, town,
+                street, streetNumber, ZIP, country );
+
+        assertEquals(contact.getTown(), town);
+        assertEquals(contact.getStreet(), street);
+        assertEquals(contact.getStreetNumber(), streetNumber);
+        assertEquals(contact.getZIP(), ZIP);
+        assertEquals(contact.getCountry(), country);
+        assertEquals(contact.getPhone(), phone);
+        assertEquals(contact.getEmail(), email);
+    }
+
+    @Test
+    public void correctly_instantiate_ContactDetailsTheater(){
+
+        String town = "Gotha";
+        String street = "Ekhofpl.";
+        int streetNumber =  3;
+        String ZIP = "99867";
+        String country = "Deutschland";
+        String phone = "+493621510430";
+        String email = "info@gth-theater.de";
+
+        ContactDetails contact = new ContactDetails(phone, email, town,
+                street, streetNumber, ZIP, country );
+
+        assertEquals(contact.getTown(), town);
+        assertEquals(contact.getStreet(), street);
+        assertEquals(contact.getStreetNumber(), streetNumber);
+        assertEquals(contact.getZIP(), ZIP);
+        assertEquals(contact.getCountry(), country);
+        assertEquals(contact.getPhone(), phone);
+        assertEquals(contact.getEmail(), email);
+    }
+
+    @Test
+    public void correctly_instantiate_ContactDetailsRegierung(){
+
+        String town = "Bonn";
+        String street = "Adenaueralee";
+        int streetNumber =  139;
+        String ZIP = "53113";
+        String country = "Deutschland";
+        String phone = "0228 560";
+        String email = "bundes@kanzleramt.de";
+
+        ContactDetails contact = new ContactDetails(phone, email, town,
+                street, streetNumber, ZIP, country );
+
+        assertEquals(contact.getTown(), town);
+        assertEquals(contact.getStreet(), street);
+        assertEquals(contact.getStreetNumber(), streetNumber);
+        assertEquals(contact.getZIP(), ZIP);
+        assertEquals(contact.getCountry(), country);
+        assertEquals(contact.getPhone(), phone);
+        assertEquals(contact.getEmail(), email);
+    }
+
+    @Test
+    public void correctly_instantiate_ContactDetailsIbm(){
+
+        String town = "Düsseldorf";
+        String street = "Karl-Arnold-Platz";
+        int streetNumber =  1;
+        String ZIP = "40474";
+        String country = "Deutschland";
+        String phone = "+49211 4760";
+        String email = "contact@ibm.com";
+
+        ContactDetails contact = new ContactDetails(phone, email, town,
+                street, streetNumber, ZIP, country );
+
+        assertEquals(contact.getTown(), town);
+        assertEquals(contact.getStreet(), street);
+        assertEquals(contact.getStreetNumber(), streetNumber);
+        assertEquals(contact.getZIP(), ZIP);
+        assertEquals(contact.getCountry(), country);
+        assertEquals(contact.getPhone(), phone);
+        assertEquals(contact.getEmail(), email);
+    }
+
+    @Test
+    public void correctly_instantiate_ContactDetailsFerrero(){
+
+        String town = "Frankfurt";
+        String street = "Hainer Weg";
+        int streetNumber =  120;
+        String ZIP = "60599";
+        String country = "Deutschland";
+        String phone = "069 / 68 05 - 0";
+        String email = "consumerservice@ferrero.com";
+
+        ContactDetails contact = new ContactDetails(phone, email, town,
+                street, streetNumber, ZIP, country );
+
+        assertEquals(contact.getTown(), town);
+        assertEquals(contact.getStreet(), street);
+        assertEquals(contact.getStreetNumber(), streetNumber);
+        assertEquals(contact.getZIP(), ZIP);
+        assertEquals(contact.getCountry(), country);
+        assertEquals(contact.getPhone(), phone);
+        assertEquals(contact.getEmail(), email);
+    }
+
+    @Test
+    public void correctly_instantiate_ContactDetailsHortusBotanicus(){
+
+        String town = "Leiden";
+        String street = "Rapenburg";
+        int streetNumber =  73;
+        String ZIP = "2311";
+        String country = "Niederlande";
+        String phone = "071 5275144";
+        String email = "hortus@hortus.leidenuniv.nl";
 
         ContactDetails contact = new ContactDetails(phone, email, town,
                 street, streetNumber, ZIP, country );
