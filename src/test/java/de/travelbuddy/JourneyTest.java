6ffd868ae862f8c334bd490bc2b70d95ae96ed32
Freed40 <friederullmann@yahoo.de>
2020-01-29 02:12:56 +0100

    JourneyTest now it work

--- src/test/java/de/travelbuddy/JourneyTest.java
+++ src/test/java/de/travelbuddy/JourneyTest.java
@@ -1,27 +1,30 @@
 package de.travelbuddy;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import de.travelbuddy.place.Sight;
 import org.junit.jupiter.api.Test;
-import de.travelbuddy.*;
+import de.travelbuddy.Person;
 import de.travelbuddy.place.Place;
 
 import java.time.LocalDate;
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Currency;
 import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
+
 public class JourneyTest {
+
     @Test
     public void correctly_instantiate_journey(){
 
         //Given
-        string title    = "Berlin";
+        String title = "Berlin";
 
         ContactDetails contact = new ContactDetails("030115", "brandenburger@tor.berlin", "Berlin",
                 "Pariser Platz", 1, "10117", "Germany" );
@@ -30,16 +33,16 @@ public class JourneyTest {
         ContactDetails contact2 = new ContactDetails("015777820000", "frieder.ullmann@fh-erfurt.de", "Erfurt",
                 "Friedrich-Engels Str.", 13, "99086", "Germany" );
 
-        Person person = new Person("Frieder", "Ullmann", bday, contact2);
 
-        Place place = new Place("Brandenburgertor", new Coordinates("52.516275", "13.377704"), contact,
+        Place place = new Sight("Brandenburgertor", new Coordinates("52.516275", "13.377704"), contact,
                 LocalDateTime.now(), LocalDateTime.now().plusHours(5), new ArrayList<>(),
-                new ArrayList<>(), new ArrayList<>(ArrayList.List(person)), false);
+                new ArrayList<>(), new ArrayList<>(), false);
 
+        Person person = new Person("Frieder", "Ullmann", bday, contact2);
 
 
         //When
-        Journey journey = new Journey(title, new ArrayList<>(ArrayList.asList(place)), new ArrayList<>(ArrayList.asList(person)));
+        Journey journey = new Journey(title, new ArrayList<>(), new ArrayList<>());
 
 
 
