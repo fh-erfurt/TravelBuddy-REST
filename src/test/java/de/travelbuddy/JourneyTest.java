3f24e0120281107dce26effc4d7f86978adc240d
Freed40 <friederullmann@yahoo.de>
2020-01-29 01:32:34 +0100

    JourneyTest

--- src/test/java/de/travelbuddy/JourneyTest.java
+++ src/test/java/de/travelbuddy/JourneyTest.java
@@ -3,11 +3,9 @@ package de.travelbuddy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import de.travelbuddy.place.Place;
-import de.travelbuddy.place.Sight;
 import org.junit.Test;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -25,18 +23,22 @@ public class JourneyTest {
         ContactDetails contact = new ContactDetails("030115", "brandenburger@tor.berlin", "Berlin",
                 "Pariser Platz", 1, "10117", "Germany" );
 
-        Place place = new Sight("Brandenburgertor", new Coordinates("52.516275", "13.377704"), contact,
-                LocalDateTime.now(), LocalDateTime.now().plusHours(5), new ArrayList<>(),
-                new ArrayList<>(), new ArrayList<>(), false);
-
         LocalDate bday = LocalDate.of(1995,7,8);
         ContactDetails contact2 = new ContactDetails("015777820000", "frieder.ullmann@fh-erfurt.de", "Erfurt",
                 "Friedrich-Engels Str.", 13, "99086", "Germany" );
 
         Person person = new Person("Frieder", "Ullmann", bday, contact2);
 
+        Place place = new Place("Brandenburgertor", new Coordinates("52.516275", "13.377704"), contact,
+                LocalDateTime.now(), LocalDateTime.now().plusHours(5), new ArrayList<>(),
+                new ArrayList<>(), new ArrayList<>(ArrayList.List(person)), false);
+
+
+
         //When
         Journey journey = new Journey(title, new ArrayList<>(ArrayList.asList(place)), new ArrayList<>(ArrayList.asList(person)));
+
+
 
         //Then
         assertEquals(journey.getTitle(), title);
