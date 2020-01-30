e258b1b7a0ee36531083a84e3e1ed6bd9f46e2f1
Freed40 <friederullmann@yahoo.de>
2020-01-30 10:45:58 +0100

    PersonTest

--- src/test/java/de/travelbuddy/PersonTest.java
+++ src/test/java/de/travelbuddy/PersonTest.java
@@ -27,7 +27,33 @@ public class PersonTest {
 
         //Then
         assertEquals(person.getFirstName(), firstname);
+        assertEquals(person.getFirstName(), "Marcel");
         assertEquals(person.getName(), name);
         assertEquals(person.getBirthdate(), bday);
+        assertEquals(person.getContactDetails(), contact);
+    }
+    @Test
+    public void false_date_person(){
+        //Given
+        String firstname = "Frieder";
+        String name = "Ullmann";
+        LocalDate bday = LocalDate.of(1995,7,8);
+        String town = "Erfurt";
+        String street = "Friedrich-Engels Str.";
+        int streetNumber =  13;
+        String ZIP = "99086";
+        String country = "Deutschland";
+        String phone = "036167000";
+        String email = "rektorat@fh-erfurt.de";
+        ContactDetails contact = new ContactDetails(phone, email, town,street, streetNumber,ZIP,country );
+
+        //When
+        Person person = new Person(firstname, name, bday, contact);
+        Person person2 = new Person("Marcel",name, bday, contact);
+
+        //Then
+        assertNotEquals(person2.getFirstName(), person.getFirstName());
+        assertNotEquals(person2.getFirstName(), name);
+        assertEquals(person.getFirstName(), firstname);
     }
 }
