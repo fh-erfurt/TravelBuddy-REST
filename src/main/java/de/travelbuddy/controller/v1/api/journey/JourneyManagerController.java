41492ad624daa85fbf59816b519ef23f5848e5ee
FlyingDutchman <Nm2Fw7$8MxN@CK%R!6xM*xz>
2020-06-14 17:32:32 +0200

    Added Swagger UI

--- src/main/java/de/travelbuddy/controller/v1/api/journey/JourneyManagerController.java
+++ src/main/java/de/travelbuddy/controller/v1/api/journey/JourneyManagerController.java
@@ -1,8 +1,12 @@
 package de.travelbuddy.controller.v1.api.journey;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/manager")
 public class JourneyManagerController {
+
 
 }
