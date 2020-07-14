d8bbaf466fb64774035dee072020b5e8552068da
Marcel van der Heide <flyingdutchman@dutches.de>
2020-07-14 21:12:51 +0200

    Start cleanup

--- src/main/java/de/travelbuddy/storage/core/JpaGenericStream.java
+++ src/main/java/de/travelbuddy/storage/core/JpaGenericStream.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Component;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
+/**
+ * Allows the retrieval of data with complex queries
+ * @param <T> Type class of the model
+ */
 @Component
 public class JpaGenericStream<T> implements IJpaGenericStream<T> {
 
     private Class<T> type;
     private final EntityManager entityManager;
-    private JinqJPAStreamProvider streams;
+    private JinqJPAStreamProvider streams = null;
     private EntityManagerFactory emf = DataController.getInstance().getEntityManagerFactory();
 
     public JpaGenericStream()
@@ -32,6 +36,10 @@ public class JpaGenericStream<T> implements IJpaGenericStream<T> {
         return type;
     }
 
+    /**
+     * Create a stream for the model T
+     * @return A stream to query
+     */
     public JinqStream<T> getStream()
     {
         return streams.streamAll(entityManager, type);
